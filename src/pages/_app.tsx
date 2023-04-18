be3139d833574292e0b4163a112aa648f7eab00b
Joona Viertola <joona.viertola@houston-inc.com>
2023-04-18 14:07:33 +0300

    Load google font using Next.js's own method

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { Fira_Sans } from "next/font/google";
+
+const firaSans = Fira_Sans({ subsets: ["latin"], weight: ["400", "600"] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${firaSans.style.fontFamily};
+        }
+      `}</style>
+      <Component {...pageProps} />
+    </>
+  );
 }
