482eca8733eb51c11b8d9ea481d180ab4fe6352f
Joona Viertola <joona.viertola@houston-inc.com>
2023-04-23 16:52:55 +0300

    Add consent block and show logic for comments

--- src/components/CommentBlock.tsx
+++ src/components/CommentBlock.tsx
@@ -13,25 +13,53 @@ export default function CommentBlock({}: CommentBlockProps) {
     <>
       <section className="comment-section">
         <h1>Comments</h1>
+
+        <article className="comment-section__consent-block" id="consentBlock">
+          <p>Would you like to see comments?</p>
+
+          <p>
+            Comments are hidden by default, since they use 3rd party service{" "}
+            <a href="https://disqus.com/">Disqus</a>.
+          </p>
+
+          <button
+            className="comment-section__show-button"
+            id="showCommentsButton"
+          >
+            Show comments
+          </button>
+        </article>
         <div id="disqus_thread"></div>
       </section>
 
       <Script id="disqus" strategy="lazyOnload">
         {`
-            /*
-            var disqus_config = function () {
-            this.page.url = ${canonicalUrl};
-            this.page.identifier = 'joonaviertola';
-            };
-            */
+            var button = document.getElementById('showCommentsButton');
+            var consentBlock = document.getElementById('consentBlock');
 
-            (function() { // DON'T EDIT BELOW THIS LINE
-            var d = document, s = d.createElement('script');
-            s.src = 'https://joonaviertola.disqus.com/embed.js';
-            s.setAttribute('data-timestamp', +new Date());
-            (d.head || d.body).appendChild(s);
-            })();
-          `}
+            button.addEventListener('click', function() {
+                triggerDisqus();
+                hideConsentBlock();
+            }, false);
+
+            function hideConsentBlock() {
+                consentBlock.remove();
+            }
+
+            function triggerDisqus() {
+                var disqus_config = function () {
+                    this.page.url = '${canonicalUrl}';
+                    this.page.identifier = 'joonaviertola';
+                };
+
+                (function() { // DON'T EDIT BELOW THIS LINE
+                var d = document, s = d.createElement('script');
+                s.src = 'https://joonaviertola.disqus.com/embed.js';
+                s.setAttribute('data-timestamp', +new Date());
+                (d.head || d.body).appendChild(s);
+                })();
+            }
+        `}
       </Script>
     </>
   );
